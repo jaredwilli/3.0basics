86ac5d2946c737c2711e5cb6864e9bf8f3e174de
Jared Williams <jaredwilli@gmail.com>
2010-07-30 16:28:15 -0400

    footer nav menu added

--- footer.php
+++ footer.php
@@ -1,16 +1,17 @@
-<div id="footer">
+	<div id="footer">
 
-	<div id="site-info" class="inner clearfix">
+		<div id="site-info" class="inner clearfix">
+	
+			<?php wp_nav_menu( array( 'menu' => 'Footer', 'class' => '', 'container' => false, 'theme_location' => 'Footer Navigation' )); ?>
+			
+			<p id="copyright">&copy; <?php echo date("Y"); ?> 
+			<?php bloginfo('name'); ?>. Site Design by <a href="http://new2wp.com">New2WP</a></p>
+		</div>
 
-		<p id="copyright">&copy; <?php echo date("Y"); ?> 
-		<?php bloginfo('name'); ?>. Site Design by <a href="http://new2wp.com">New2WP</a></p>
-	</div>
-
-</div><!--// end #footer -->
+	</div><!--// end #footer -->
 </div><!--// end #wrapper -->
 
 <?php wp_footer(); ?>
-
 
 </body>
 </html>
