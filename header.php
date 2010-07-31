164dcb1eea92f36aea5644def471241b3713117a
Jared Williams <jaredwilli@gmail.com>
2010-07-30 22:31:40 -0400

    put js files in footer

--- header.php
+++ header.php
@@ -7,10 +7,7 @@
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link href="<?php bloginfo('template_url'); ?>/css/print.css" type="text/css" rel="stylesheet" media="print" />
 <!--[if IE]><link href="<?php bloginfo('template_url'); ?>/css/ie.css" type="text/css" rel="stylesheet" media="all" /><![endif]-->
-<!--<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/cufon-yui.js"></script>-->
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 <?php wp_head(); ?>
-<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/global.js"></script>
