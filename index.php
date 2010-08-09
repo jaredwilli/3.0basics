233eb27c1ddb3908b4a4ecaa3fc9a8700b9ce8f8
Jared Williams <jaredwilli@gmail.com>
2010-08-08 20:43:23 -0400

    replaced main loop with get_template_part() to put all reusable code for loops in one file loop.php

--- index.php
+++ index.php
@@ -4,37 +4,10 @@
 <?php include( TEMPLATEPATH . '/branding.php' ); ?>
 
 <div id="content" class="inner clearfix">
-	<div id="content-main">
-	<?php if ( have_posts()) : while (have_posts() ) : the_post(); ?>
-	
-		<div <?php post_class('clearfix'); ?> id="post-<?php the_ID(); ?>">
-			<h1 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-			<ul class="postinfo clearfix">
-				<li class="authordata"><?php the_time('M j, Y'); ?><!-- at <?php the_time(); ?>--> by <strong><?php the_author(); ?></strong></li>
-				<li class="commentdata"><?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?></li>
-			</ul>
-			<div class="entry clearfix">
-				<?php the_content(__('Read more')); ?>
-			</div><!--// end #entry -->
-				<div class="fl post-tags"><?php the_tags( __( ' ' ), ' , ', ' ' ); ?></div>
-				<div class="fr edit-link"><?php edit_post_link( __( 'Edit' ) ); ?></div>
-		</div><!--// end #post-<?php the_ID(); ?> -->
-	
-	<?php endwhile; ?>
-	
-		<?php if( function_exists( 'wp_pagenavi' )) { wp_pagenavi(); } else { ?>
-		<div class="navigation clearfix">
-			<?php wp_link_pages(); ?>
-			<div class="alignleft"><?php next_posts_link('&laquo; Previous Entries'); ?></div>
-			<div class="alignright"><?php previous_posts_link('Next Entries &raquo;'); ?></div>
-		</div>
-		<?php } ?>
+	<div id="content-main" role="main">
 
-	<?php else :
-		// Show 404 message
-		if (function_exists('bb_404')) { bb_404(); }
-	endif; ?>
-	
+	<?php get_template_part( 'loop', 'index' ); ?>
+
 	</div><!-- end #content-main -->
 	<div id="content-sub">
 		<?php get_sidebar(); ?>
